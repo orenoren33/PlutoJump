78c120741f4d90980b631aa86a274ea2e410d1dd
Oren <orenorentk@gmail.com>
2025-06-25 11:07:01 -0700

    First files

---
+++ Assets/DeathScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class DeathScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { }
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.layer == 6)
+            {
+                
+            }
+        }
+    }
+
---
+++ Assets/PlutoMoveScript.cs
@@ -0,0 +1,43 @@
+using Unity.VisualScripting;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+
+public class PlutoMoveScript : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody;
+    public PolygonCollider2D playerCollider;
+    public float goRightPower;
+    public float goLeftPower;
+    public float goUpPower;
+    private Vector3 startPosision;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPosision = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            myRigidbody.linearVelocity = Vector2.right * goRightPower;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            myRigidbody.linearVelocity = Vector2.left * goLeftPower;
+        }
+
+        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && playerCollider.IsTouchingLayers())
+        {
+            myRigidbody.linearVelocity = Vector2.up * goUpPower;
+        }
+
+        if (transform.position.y <= -10)
+        {
+            transform.position = startPosision;
+        }
+    }
+}
